77a6b6f4a73554178f12e86103988623f96843d0
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-05-22 13:12:40 -0700

    repro

---
+++ src/mastra/index.ts
@@ -0,0 +1,20 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { workflow1 } from './workflows/weather-workflow';
+import { weatherAgent } from './agents/weather-agent';
+
+export const mastra = new Mastra({
+  workflows: { workflow1 },
+  agents: { weatherAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+
+});
